3109f62ca71115da032de90c22b5457c223183db
Maik Klein <maikklein@googlemail.com>
2013-12-24 17:12:55 +0100

    Added Physics

---
+++ unityfslib/unityfslib/Physics.fs
@@ -0,0 +1,10 @@
+﻿[<RequireQualifiedAccess>]
+module FSharp.Unity3D.Physics
+
+open UnityEngine
+
+let RayCast ray dist =
+    let mutable r = new RaycastHit()
+    match Physics.Raycast(ray,&r,dist) with
+       | true  -> Some(r)
+       | false -> None
