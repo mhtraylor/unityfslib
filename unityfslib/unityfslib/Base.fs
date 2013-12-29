39cb51c024496e033c4e2f477efa3f46812c0a97
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2013-12-28 20:31:26 -0600

    Add Array extension, fix function name.

--- unityfslib/unityfslib/Base.fs
+++ unityfslib/unityfslib/Base.fs
@@ -1,7 +1,16 @@
 ﻿[<AutoOpen>]
 module FSharp.Unity3D.Base
 
+
+// Module extensions.
+module Array =
+
+    // In-place array modification, useful for Mesh vertices, Particles, etc.
+    let apply f a =
+        a |> Array.iteri (fun i x -> a.[i] <- f x)
+        a
+
 module Option =
 
-    let rev v = function true -> Some v | _ -> None
+    let fromBool v = function true -> Some v | _ -> None
 
