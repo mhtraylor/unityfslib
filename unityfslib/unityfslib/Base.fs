5bc07aa68d0666837b2ab79c842ac4c9c9ced354
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2014-01-15 00:18:34 -0600

    Change Option.fromBool to Option.cond.

--- unityfslib/unityfslib/Base.fs
+++ unityfslib/unityfslib/Base.fs
@@ -6,11 +6,11 @@ module FSharp.Unity3D.Base
 module Array =
 
     // In-place array modification, useful for Mesh vertices, Particles, etc.
-    let apply f a =
+    let modify f a =
         a |> Array.iteri (fun i x -> a.[i] <- f x)
         a
 
 module Option =
 
-    let fromBool v = function true -> Some v | _ -> None
+    let cond v = function true -> Some v | _ -> None
 
