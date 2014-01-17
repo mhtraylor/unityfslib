a84bf7823e0cd1fecafafe2d0e02f68fca5144c4
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2014-01-17 00:40:20 -0600

    Fix GetComponent calls to be more idiomatic.

--- unityfslib/unityfslib/GameObject.fs
+++ unityfslib/unityfslib/GameObject.fs
@@ -6,22 +6,28 @@ open UnityEngine
 type t = GameObject
 type u = Component
 
-let add<'a when 'a :> u> (g:t) = g.AddComponent<'a>()
+let empty = Unchecked.defaultof<t>
 
-let get<'a when 'a :> u> (g:t) = Object.exists <| g.GetComponent<'a>()
+let add<'a when 'a :> u> (g:t) = 
+    g.AddComponent<'a>()
 
-let getInChild<'a when 'a :> u> (g:t) = Object.exists <| g.GetComponentInChildren<'a>()
+let get<'a when 'a :> u> (g:t) = 
+    g.GetComponent<'a>() |> Object.exists    
 
+let getInChild<'a when 'a :> u> (g:t) = 
+    g.GetComponentInChildren<'a>() |> Object.exists
+
+// FIX -- GetComponents can return null array!
 let getAll<'a when 'a :> u> (g:t) = 
-    g.GetComponents<'a>() 
-    |> Array.map Object.exists
-    |> Array.filter Option.isSome
+    g.GetComponents<'a>()
+    |> Seq.choose Object.exists
 
-let getAllInChild<'a when 'a :> u> (g:t) = g.GetComponentsInChildren<'a>() |> Array.map Object.exists
+// FIX -- GetComponents can return null array!
+let getAllInChild<'a when 'a :> u> (g:t) = 
+    g.GetComponentsInChildren<'a>() 
+    |> Seq.choose Object.exists
 
 let activate (g:t) = g.SetActive(true)
 
 let deactivate (g:t) = g.SetActive(false)
-
-let empty = Unchecked.defaultof<t>
 
