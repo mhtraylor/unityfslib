45f1d4472aa993d01f13f80c8501256f96798bef
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2014-01-16 23:59:47 -0600

    Fix bug with reference equality.

--- unityfslib/unityfslib/Object.fs
+++ unityfslib/unityfslib/Object.fs
@@ -5,22 +5,15 @@ open UnityEngine
 
 type t = UnityEngine.Object
 
+let empty = Unchecked.defaultof<t>
+
+let exists (o:#t) =  o.Equals(null) |> not |> Option.cond o
+
 let find<'a when 'a :> t> () = Object.FindObjectOfType<'a>()
 
 let findAll<'a when 'a :> t> () = 
     Object.FindObjectsOfType<'a>()
-    |> Seq.cast<'a>
 
 let instantiate (o:#t) = Object.Instantiate(o)
 
 let instantiateAt v q (o:#t) = Object.Instantiate(o,v,q)
-
-
-let empty = Unchecked.defaultof<t>
-
-let exists (o:#t) = if obj.ReferenceEquals(o,null) then None else Some o
-
-
-
-         
-
