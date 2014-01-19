0dc45a81288852ff5355ef895d8a0eb268c74ec6
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2014-01-19 14:58:58 -0600

    Delete Plane.fs

--- unityfslib/unityfslib/Plane.fs
+++
@@ -1,9 +0,0 @@
-﻿[<RequireQualifiedAccess>]
-module FSharp.Unity3D.Plane
-
-open UnityEngine
-
-let upward pos = Plane (Vector3.up, inPoint=pos)
-
-let raycast ray (plane:Plane) = plane.Raycast(ray) |> Option.pout
-
